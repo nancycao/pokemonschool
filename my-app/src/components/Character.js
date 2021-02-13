2de58b4e7e0e94571af0b2c3017639a498e7c66f
nancycao <nancycao717@gmail.com>
2021-02-13 12:50:18 -0800

    namelist isn't saved for some reason :(

--- my-app/src/components/Character.js
+++ my-app/src/components/Character.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactModal from 'react-modal';
+import TextLoop from "react-text-loop";
+import '../CSS/Character.css'
 
 class Character extends React.Component {
     constructor(props) {
@@ -7,6 +9,7 @@ class Character extends React.Component {
 
       this.state = {
         showModal: false,
+        showGreetings: false,
         nameList: [],
       };
 
@@ -14,6 +17,7 @@ class Character extends React.Component {
       this.handleCloseModal = this.handleCloseModal.bind(this);
       this.handleNamesChange = this.handleNamesChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
+      this.handleShowGreetings = this.handleShowGreetings.bind(this);
     }
 
     handleOpenModal () {
@@ -25,26 +29,41 @@ class Character extends React.Component {
     }
 
     handleNamesChange (event) {
-      this.setState ( {nameList: event.target.value.split()} );
-
+      var nameString = event.target.value;
+      if (nameString) {
+        this.setState ( {nameList: nameString.split(",")} );
+      }
     }
 
     handleSubmit(event) {
       console.log (this.state.nameList);
     }
 
+    handleShowGreetings() {
+      console.log (this.state);
+      //this.setState ({showGreetings: true});
+    }
+
     render() {
       return (
         <div>
+          <button onClick={this.handleShowGreetings}> </button>
+
+          Welcome <TextLoop children={["nancy", "bob"]} />
+
           <img src="/dashboard/squirtle.gif" alt="pokemon" onClick={this.handleOpenModal} />
           <ReactModal isOpen={this.state.showModal}>
 
             <button onClick={this.handleCloseModal}>X</button>
 
             <form onSubmit={this.handleSubmit}>
-              <input type="text" value={this.state.names} onChange={this.handleNamesChange}/>
-
-              <input type="submit" value="Submit" onClick = {this.handleSubmit}/>
+              <div>
+                <label> Enter names seperated by commas: </label>
+                <input type="text" value={this.state.names} onChange={this.handleNamesChange}/>
+              </div>
+              <div>
+                <input type="submit" value="Submit" onClick = {this.handleSubmit}/>
+              </div>
             </form>
           </ReactModal>
 
