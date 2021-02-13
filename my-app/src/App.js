3e5ad7c942b31ac5d90008070617991da9de6267
Richard Lin <richard.lin.047@berkeley.edu>
2021-02-13 15:02:39 -0500

    Add basic assets and css for dashboard

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import Character from './components/Character.js'
+import homeButton from './assets/dashboard/home_button.png';
+import classPeriod from './assets/dashboard/class_period.png';
+import xpBar from './assets/dashboard/xp_bar.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="dashboard">
+        <button type="submit" className='home-button'><img src={homeButton} alt="Submit"/></button>
+        <div className="class-period"><img src={classPeriod}  alt="class period"/></div>
+        <img src={xpBar} className="xp-bar" alt="class period" />                              
         <Character />
-      </header>
+      </div>
     </div>
   );
 }
