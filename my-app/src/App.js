c83929b901d3a1c3f2375081d820e1677ebb71a2
nancycao <nancycao717@gmail.com>
2021-02-13 14:56:03 -0800

    progress bar bootstrap setup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,8 @@ import classPeriod from './assets/dashboard/class_period.png';
 import xpBar from './assets/dashboard/xp_bar.png';
 import endClassButton from './assets/dashboard/end_class_button.png';
 import pikachu from './assets/dashboard/pikachu.png';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
@@ -14,6 +16,10 @@ function App() {
         <img src={homeButton} className='home-button' alt="Home"/>
         <div className="class-period"><img src={classPeriod} className="class-period" alt="class period"/></div>
         <img src={xpBar} className="xp-bar" alt="experience bar" />
+
+        // documentation for ProgressBar API
+        // https://react-bootstrap.github.io/components/progress/
+        <ProgressBar animated now={45} />
 
         <div className="iframe">
           <iframe
